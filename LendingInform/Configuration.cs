715e6759efdeeadfb9a08d28aaff09247068a41e
NikitaZimin20 <90403463+NikitaZimin20@users.noreply.github.com>
2022-06-06 12:41:51 +0300

    Update Configuration.cs

--- LendingInform/Configuration.cs
+++ LendingInform/Configuration.cs
@@ -2,6 +2,6 @@
 {
     internal class Configuration
     {
-        internal static string BotToken = "5104216844:AAGlDP3xphRbhlyf3E8HQk6twCz7n9qG5Eo";
+        internal static string BotToken = "";
     }
 }
